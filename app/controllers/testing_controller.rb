3afe2e529416a4b57c3189ac2513ea9e993a4711
norrard148 <norrard@likelion.org>
2018-09-13 13:16:21 +0900

    6

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -1,10 +1,17 @@
 class TestingController < ApplicationController
   def first
   end
+  def second
+  end
 
   def first1
     respond_to do |format|               
       format.js
     end        
   end 
+
+  private
+  def t_params
+    params.require(:first).permit(:target)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'testing/first'
+  get 'testing/second'
   get 'testing/first1'
   root 'testing#first'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
