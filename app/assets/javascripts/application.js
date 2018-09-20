66dd56926fd31ac8f60b32f4812ab4a1767929d2
norrard148 <norrard@likelion.org>
2018-09-20 16:46:17 +0900

    Ss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,81 +24,128 @@
 
 
 $(document).on('scroll', function (){
-    var body = $("body");
     if ($(".index").length>0) {
         var bottom_of_object1 = $(".hideme").offset().top;
-        var bottom_of_object2 = $(".index-middle").offset().top;
-        var bottom_of_object3 = $(".index-bottom").offset().top ;
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         if( bottom_of_window > bottom_of_object1 ){
+            var bottom_of_object2 = $(".index-middle").offset().top;
+           
+            var bottom_of_object2_1 = $(".mobile.card1").offset().top+$(".mobile.card1").outerHeight();
+            var bottom_of_object2_2 = $(".mobile.card2").offset().top+$(".mobile.card2").outerHeight();
+            var bottom_of_object2_3 = $(".mobile.card3").offset().top+$(".mobile.card3").outerHeight();
             $('.hideme').animate({'opacity':'1'},500)
             if( bottom_of_window > bottom_of_object2 ){
-                $('.card1').animate({'opacity':'1'},500).queue(function() {
-                        $('.card2').animate({'opacity':'1'},500).queue(function() {
-                            $('.card3').animate({'opacity':'1'},500)
-                            $('.hideme2').animate({'opacity':'1'},500)
+                $('.pc.card1').animate({'opacity':'1'},500).queue(function() {
+                        $('.pc.card2').animate({'opacity':'1'},500).queue(function() {
+                            $('.pc.card3').animate({'opacity':'1'},500)
+                            $('.pc.hideme2').animate({'opacity':'1'},500)
                         });
                 });
-                if( bottom_of_window > bottom_of_object3 + 200 ){
-                 $(".index-bottom h1, .index-bottom h2, .index-bottom button").addClass("show");
+                if (bottom_of_window > bottom_of_object2_1) {
+                    $('.mobile.card1').animate({'opacity':'1'},500)
+                    if (bottom_of_window > bottom_of_object2_2) {
+                        $('.mobile.card2').animate({'opacity':'1'},500)
+                        if (bottom_of_window > bottom_of_object2_3) {
+                            $('.mobile.card3').animate({'opacity':'1'},500).queue(function() {
+                                $('.mobile.hideme2').animate({'opacity':'1'},500)
+                            })
+                        }
+                    }
+                }
+                var bottom_of_object3 = $(".index-bottom").offset().top ;
+                if( bottom_of_window > bottom_of_object3){
+                    $(".index-bottom h1, .index-bottom h2, .index-bottom button").addClass("show");
                 }; 
             }
+
         }
     }
-    else if ($(".aboutus-middle").length>0) {
-            var bottom_of_object0 = $(".index-middle.aboutus").offset().top;
-            var bottom_of_object1 = $(".aboutus-middle").offset().top;
-            var bottom_of_object2 = $(".aboutus-middle2").offset().top;
-            var bottom_of_object3 = $(".index-bottom.aboutus").offset().top;
+    else if ($(".aboutus").length>0) {
+            var bottom_of_object0 = $(".index-middle.aboutus").offset().top+200;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_object0 ){
-                $('.card1').animate({'opacity':'1'},500).queue(function() {
-                    $('.card2').animate({'opacity':'1'},500).queue(function() {
-                        $('.card3').animate({'opacity':'1'},500);
+                $('.hideme').animate({'opacity':'1'},500);
+                $('.pc.card1').animate({'opacity':'1'},500).queue(function() {
+                    $('.pc.card2').animate({'opacity':'1'},500).queue(function() {
+                        $('.pc.card3').animate({'opacity':'1'},500);
                     });
                 });
-            };
-            if( bottom_of_window > bottom_of_object1 ){
-                $(".aboutus-middle").addClass("show");
-            
-            };
-            if( bottom_of_window > bottom_of_object2 ){
-                $('.card4').animate({'opacity':'1'},500).queue(function() {
-                    $('.card5').animate({'opacity':'1'},500).queue(function() {
-                        $('.card6').animate({'opacity':'1'},500).queue(function() {
-                            $('.card7').animate({'opacity':'1'},500)
-                        });;
-                    });
-                });
-            
-            };
-            if( bottom_of_window > bottom_of_object3 ){
-                $(".index-bottom.aboutus h1, .index-bottom.aboutus button").addClass("show");
+                var bottom_of_object0_1 = $(".mobile.card1").offset().top+$(".mobile.card1").outerHeight();
+                var bottom_of_object0_2 = $(".mobile.card2").offset().top+$(".mobile.card1").outerHeight();
+                var bottom_of_object0_3 = $(".mobile.card3").offset().top+$(".mobile.card1").outerHeight();
+                if (bottom_of_window > bottom_of_object0_1) {
+                    $('.mobile.card1').animate({'opacity':'1'},500)
+                }
+                if (bottom_of_window > bottom_of_object0_2) {
+                    $('.mobile.card2').animate({'opacity':'1'},500)
+                }
+                if (bottom_of_window > bottom_of_object0_3) {
+                    $('.mobile.card3').animate({'opacity':'1'},500)
+                }
+                var bottom_of_object1_1 = $(".aboutus-middle-box-left").offset().top;
+                var bottom_of_object1_2 = $(".aboutus-middle-box-right").offset().top;
+                if( bottom_of_window > bottom_of_object1_1 ){
+                    $('.aboutus-middle-box-left').animate({'opacity':'1'},500)
+                    $('.hideme2').animate({'opacity':'1'},500)
+                    if (bottom_of_window > bottom_of_object1_2) {
+                        $('.aboutus-middle-box-right').animate({'opacity':'1'},500)
+                        var bottom_of_object2 = $(".aboutus-middle2").offset().top;
+                        if( bottom_of_window > bottom_of_object2 ){
+                            var bottom_of_object2_1 = $(".mobile.card4").offset().top +$(".mobile.card4").outerHeight();
+                            var bottom_of_object2_2 = $(".mobile.card5").offset().top +$(".mobile.card5").outerHeight();
+                            var bottom_of_object2_3 = $(".mobile.card6").offset().top +$(".mobile.card6").outerHeight();
+                            var bottom_of_object2_4 = $(".mobile.card7").offset().top +$(".mobile.card7").outerHeight();
+                            $('.pc.card4').animate({'opacity':'1'},500).queue(function() {
+                                $('.pc.card5').animate({'opacity':'1'},500).queue(function() {
+                                    $('.pc.card6').animate({'opacity':'1'},500).queue(function() {
+                                        $('.pc.card7').animate({'opacity':'1'},500)
+                                    });;
+                                });
+                            });
+                            if (bottom_of_window > bottom_of_object2_1) {
+                                $('.mobile.card4').animate({'opacity':'1'},500)
+                                if (bottom_of_window > bottom_of_object2_2) {
+                                    $('.mobile.card5').animate({'opacity':'1'},500)
+                                    if (bottom_of_window > bottom_of_object2_3) {
+                                        $('.mobile.card6').animate({'opacity':'1'},500)
+                                        if (bottom_of_window > bottom_of_object2_4) {
+                                            $('.mobile.card7').animate({'opacity':'1'},500)
+                                            var bottom_of_object3 = $(".index-bottom.aboutus").offset().top;
+                                            if( bottom_of_window > bottom_of_object3 ){
+                                                $(".index-bottom.aboutus h1, .index-bottom.aboutus button").addClass("show");
+                                            };
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    };
+                
+                };
             };
     }
     else if ($("#works-container1").length>0) {
-        console.log("works")
         var bottom_of_object0 = $("#works-box1").offset().top + 150;
-        var bottom_of_object1 = $("#works-box2").offset().top + 150;
-        var bottom_of_object2 = $("#works-box3").offset().top + 150;
-        var bottom_of_object3 = $("#works-bottom").offset().top;;
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         if( bottom_of_window > bottom_of_object0 ){
             $("#works-box1").addClass("draw")
             $("#works-container1 h1, #works-container1 h2").addClass("show");
-        };
-        if( bottom_of_window > bottom_of_object1 ){
-            $("#works-box1").addClass("btb-active")
-            $("#works-container2 h1, #works-container2 h2").addClass("show");
-            $("#works-box2").addClass("draw")
-        };
-        if( bottom_of_window > bottom_of_object2 ){
-            $("#works-box2").addClass("btb-active")
-            $("#works-container3 h1, #works-container3 h2").addClass("show");
-            $("#works-box3").addClass("draw")
-        };
-        if( bottom_of_window > bottom_of_object3 ){
-            $("#works-bottom, #works-bottom h1, #works-bottom h2, #works-bottom button").addClass("show");
+            var bottom_of_object1 = $("#works-box2").offset().top + 150;
+            if( bottom_of_window > bottom_of_object1 ){
+                $("#works-box1").addClass("btb-active")
+                $("#works-container2 h1, #works-container2 h2").addClass("show");
+                $("#works-box2").addClass("draw")
+                var bottom_of_object2 = $("#works-box3").offset().top + 150;
+                if( bottom_of_window > bottom_of_object2 ){
+                    $("#works-box2").addClass("btb-active")
+                    $("#works-container3 h1, #works-container3 h2").addClass("show");
+                    $("#works-box3").addClass("draw")
+                    var bottom_of_object3 = $("#works-bottom").offset().top+$("#works_bottom").outerHeight();
+                    if( bottom_of_window > bottom_of_object3 ){
+                        $("#works-bottom, #works-bottom h1, #works-bottom h2, #works-bottom button").addClass("show");
+                    };
+                };
+            };
         };
      }
 });
